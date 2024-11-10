4dd3791222d2f1ab9c786eed933e147f140bb785
NirajSalunke <154681227+NirajSalunke@users.noreply.github.com>
2024-11-10 18:53:35 +0530

    Day-1 Update

--- 09Slices/main.go
+++ 09Slices/main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 func main() {
+	var fruitList = []string{"hello", "Niraj"}
+	// fmt.Printf("%T", fruitList)
+	fruitList = append(fruitList, "banana", "mango")
+	fmt.Println(fruitList)
+	// here 0 index will be omitted ,also  3rd and above 3 will be omitted
+	fruitList = fruitList[1:3]
+	fmt.Println(fruitList)
+
+	highScore := make([]int, 4) // first arg is type of data and second its length
+	highScore[0] = 100
+	highScore[1] = 300
+	highScore[2] = 200
+	highScore[3] = 400
+
+	// highScore[4] = 500 , this will cause error as size is 4 but,
+	highScore = append(highScore, 500, 700) // this will fulfill
+
+	sort.Ints(highScore)
+	fmt.Println(sort.IntsAreSorted(highScore)) // gives bool isSorted or not
+	fmt.Println(highScore)
+
+	// How to remove a value from slice based on index
+
+	var courses = []string{"Go", "React", "JavaScript", "Swift", "Python"}
+
+	var indexOfItemToBeRemoved int = 2
+	courses = append(courses[:indexOfItemToBeRemoved], courses[indexOfItemToBeRemoved+1:]...)
+	fmt.Println(courses)
 
 }
---
+++ README.md
@@ -0,0 +1,2 @@
+Currently at Ended with 15th video
+16/57 completed
